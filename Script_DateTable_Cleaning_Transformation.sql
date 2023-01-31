1cbea988be40293c5e2813d6a26a008a5400890d
anubhutih19 <101438177+anubhutih19@users.noreply.github.com>
2023-01-31 17:00:36 -0500

    Add files via upload

---
+++ Script_DateTable_Cleaning_Transformation.sql
@@ -0,0 +1,24 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [DateKey]
+      ,[FullDateAlternateKey] AS Date
+      --,[DayNumberOfWeek]
+      ,[EnglishDayNameOfWeek] AS Day
+      --,[SpanishDayNameOfWeek]
+      --,[FrenchDayNameOfWeek]
+      --,[DayNumberOfMonth]
+      --,[DayNumberOfYear]
+      ,[WeekNumberOfYear] AS Week
+      ,[EnglishMonthName] AS Month
+	  ,LEFT([EnglishMonthName],3) AS MonthShort
+      --,[SpanishMonthName]
+      --,[FrenchMonthName]
+      ,[MonthNumberOfYear] AS MonthNo
+
+      ,[CalendarQuarter] AS Quarter
+      ,[CalendarYear] AS Year 
+      --,[CalendarSemester]
+      --,[FiscalQuarter]
+      --,[FiscalYear]
+      --,[FiscalSemester]
+  FROM [AdventureWorksDW2019].[dbo].[DimDate]
+  WHERE CalendarYear >= 2019
